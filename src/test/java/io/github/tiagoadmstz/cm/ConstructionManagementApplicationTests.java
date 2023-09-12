1010f566626b8c131f42e95066d5686c55694b50
Tiago Daniel Teixeira <tiago.teixeira@philips.com>
2023-09-12 19:44:29 -0300

    initial commit

---
+++ src/test/java/io/github/tiagoadmstz/cm/ConstructionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tiagoadmstz.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
